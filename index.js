19d3ba00cf7456f796b0f387ba70978e118df436
Artem Ivanov <fast0490f@mail.ru>
2018-12-18 17:01:40 +0300

    version 0.0.1

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const Plugin = require('./lib/plugin');
+const wol = require('wake_on_lan');
 
 const plugin = new Plugin();
 
 
 plugin.on('device_action', (device) => {
-  plugin.debug("incomming action: " + device.id + " / " + device.command);
+  wol.wake(device.id, {
+    address: device.address,
+    num_packets: device.count,
+    interval: device.interval,
+    port: device.port,
+  }, (e) => plugin.debug(e));
 })
 
 plugin.on('toolbar_command', (command) => {
   switch (command.type) {
     case 'MANUAL_WAKEONLAN':
+      plugin.debug(command);
       break;
     default:
       break;
